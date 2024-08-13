458edfa310d128387967dcf17b31b2b0727bf02d
Chandan Vishwakarma <sanyukt1994@gmail.com>
2024-08-14 00:49:47 +0530

    feat: add in-memory cache

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,35 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Inject, Param, Post } from '@nestjs/common';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+
+import { Cache } from 'cache-manager';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('cache')
+  async setToCache(
+    @Body('id') id: string,
+    @Body('message') message: string,
+  ): Promise<void> {
+    await this.cacheManager.set(id, message, 15000);
+  }
+
+  @Get('cache/:id')
+  async getFromCache(@Param('id') id: string): Promise<string> {
+    const message = await this.cacheManager.get<string>(id);
+
+    console.log(message);
+
+    return message;
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
+import { CacheModule } from '@nestjs/cache-manager';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { WebsocketModule } from './websocket';
 
 @Module({
-  imports: [WebsocketModule],
+  imports: [
+    WebsocketModule,
+    CacheModule.register({ isGlobal: true, ttl: 10 * 1000 }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
